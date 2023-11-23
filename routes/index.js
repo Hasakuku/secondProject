7c34fea984da9ceeb9d2b6fbcd7794a6cf3a44e7
wonsik <seodnjstlr@gmail.com>
2023-11-23 22:26:53 +0900

    fixed: search api

--- routes/index.js
+++ routes/index.js
@@ -9,65 +9,106 @@ router.use('/users', userRouter);
 router.use('/lodgings', lodgingRouter);
 router.use('/attractions', attractionRouter);
 router.get('/search', searchList)
-module.exports = router;
+module.exports = router
 /**
  * @swagger
  * /api/search:
  *   get:
- *     summary: 키워드를 사용하여 숙소와 관광지를 검색
+ *     summary: 키워드를 사용하여 숙소과 관광지 검색
  *     parameters:
  *       - in: query
  *         name: keyword
  *         schema:
  *           type: string
  *         example: 서울
- *         description: 검색할 키워드 
+ *         description: 검색 키워드
  *       - in: query
  *         name: item
  *         schema:
  *           type: integer
  *         example: 4
- *         description: 반환할 항목의 수
+ *         description: 반환할 항목 수
  *     responses:
  *       200:
- *         description: 검색 결과를 성공적으로 반환했습니다.
+ *         description: 검색 결과를 성곡적으로 반환
  *         content:
  *           application/json:
  *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   lodgingId:
- *                     type: string
- *                   name:
- *                     type: string
- *                   mainImage:
- *                     type: string
- *                   avgRating:
- *                     type: number
- *                   reviewCount:
- *                     type: integer
- *                   country:
- *                     type: string
- *                   address:
+ *               type: object
+ *               properties:
+ *                 lodgings:
+ *                   type: array
+ *                   items:
  *                     type: object
- *                   minPrice:
- *                     type: number
- *                   attractionId:
- *                     type: string
- *                 example:
- *                     {lodgingId: 1,
- *                      name: 호텔1,
- *                       mainImage: mainImage.jpg,
- *                       avgRating: 4.5,
- *                       reviewCount: 2,
- *                       country: KR,
- *                          address: {
- *                          city: 서울,
- *                          county: 강남구,
- *                          district: 삼성동,
- *                          detail: 테헤란로 123
- *                          },
- *                       minPrice: 100000}
+ *                     properties:
+ *                       lodgingId:
+ *                         type: string
+ *                       name:
+ *                         type: string
+ *                       mainImage:
+ *                         type: string
+ *                       avgRating:
+ *                         type: number
+ *                       reviewCount:
+ *                         type: integer
+ *                       country:
+ *                         type: string
+ *                       address:
+ *                         type: object
+ *                       minPrice:
+ *                         type: number
+ *                 attractions:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       attractionId:
+ *                         type: string
+ *                       name:
+ *                         type: string
+ *                       mainImage:
+ *                         type: string
+ *                       avgRating:
+ *                         type: number
+ *                       reviewCount:
+ *                         type: integer
+ *                       address:
+ *                         type: object
+ *                       country:
+ *                         type: string
+ *             examples:
+ *               lodgingsAndAttractions:
+ *                 value:
+ *                   {lodgings: [{
+ *                       "lodgingId": "1",
+ *                       "name": "호텔1",
+ *                       "mainImage": "mainImage.jpg",
+ *                       "avgRating": 4.5,
+ *                       "reviewCount": 2,
+ *                       "country": "KR",
+ *                       "address": {
+ *                         "city": "서울",
+ *                         "county": "강남구",
+ *                         "district": "삼성동",
+ *                         "detail": "테헤란로 123"
+ *                       },
+ *                       "minPrice": 100000
+ *                     }
+ *                   ],
+ *                   attractions: [
+ *                     {
+ *                       "attractionId": "1",
+ *                       "name": "63빌딩",
+ *                       "mainImage": "mainImage2.jpg",
+ *                       "avgRating": 4.5,
+ *                       "reviewCount": 2,
+ *                       "address": {
+ *                         "city": "서울특별시",
+ *                         "county": "영등포구",
+ *                         "district": "여의도동",
+ *                         "detail": "63로 50"
+ *                       },
+ *                       "country": "KR"
+ *                     }
+ *                   ]}
  */
